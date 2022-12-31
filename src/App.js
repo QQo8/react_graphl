024959d1369b12839491d5ea947ddfabb493b3b8
QQo8 <107598744+QQo8@users.noreply.github.com>
2022-12-31 21:55:56 +0000

    [Security] Bump ini from 1.3.5 to 1.3.7

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import React from 'react';
+
+import Header from './components/layout/Header'
+import HomeSection from './components/HomeSection';
+import ResumeSection from './components/ResumeSection';
+
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <Header />
+        <HomeSection />
+        <ResumeSection />
+      </>
+    )
+  }
+}
+
+export default App;
---
+++ src/components/HomeSection.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Jumbotron, Nav } from 'react-bootstrap';
+import { Link } from "react-scroll";
+
+import './HomeSection.css';
+
+class HomeSection extends React.Component {
+  render() {
+    return (
+      <div className="home-section" id="home"
