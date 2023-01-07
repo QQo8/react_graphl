eb82fd577783a1de570ee14e35aaee182214fc97
QQo8 <107598744+QQo8@users.noreply.github.com>
2023-01-07 11:00:33 +0000

    add compiled files

--- src/components/HomeSection.js
+++ src/components/HomeSection.js
@@ -7,4 +7,14 @@ import './HomeSection.css';
 class HomeSection extends React.Component {
   render() {
     return (
-      <div className="home-section" id="home"
+      <div className="home-section" id="home">
+        <Jumbotron>
+          <h1>Jonathan Bogdanovicz</h1>
+          <h3>
+            Creating web experiences for the next generation of consumer-facing companies
+          </h3>
+          <hr />
+          <Nav className="justify-content-center social-container">
+            <Nav.Link href="https://github.com/kamilDevguru" target="_blank">
+              <svg stroke="currentColor" fill="currentColor" viewBox="0 0 496 512" height="1em" width="1em" data-reactid="26">
+                <pa
