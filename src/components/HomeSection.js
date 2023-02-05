cb3929042baf99a6ad37b55962b36da0af8689ba
QQo8 <107598744+QQo8@users.noreply.github.com>
2023-02-05 18:18:10 +0000

    Removed unused resources.

--- src/components/HomeSection.js
+++ src/components/HomeSection.js
@@ -22,4 +22,7 @@ class HomeSection extends React.Component {
               </svg>
             </Nav.Link>
             <Nav.Link href="https://www.linkedin.com/" target="_blank">
-              <svg stroke="currentColor" fill="currentColor" viewBox="0 0 448 512" height="1em" width="1em" data-reactid="30"><path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h
+              <svg stroke="currentColor" fill="currentColor" viewBox="0 0 448 512" height="1em" width="1em" data-reactid="30"><path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z" data-reactid="31"></path></svg>
+            </Nav.Link>
+            <Nav.Link href="https://twitter.com/" target="_blank">
+              <svg stroke="currentColor" fill="currentColor" viewBox="0 0 512 512" height="1em" width="1em" data-reactid="34"><path d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-
