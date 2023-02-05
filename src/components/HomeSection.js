d10e5b8f5e62b5c3dc3bce5750cbb1b256096895
QQo8 <107598744+QQo8@users.noreply.github.com>
2023-02-05 17:05:09 +0000

    add html

--- src/components/HomeSection.js
+++ src/components/HomeSection.js
@@ -21,4 +21,5 @@ class HomeSection extends React.Component {
                 </path>
               </svg>
             </Nav.Link>
-            <Nav.Link href="https://www.li
+            <Nav.Link href="https://www.linkedin.com/" target="_blank">
+              <svg stroke="currentColor" fill="currentColor" viewBox="0 0 448 512" height="1em" width="1em" data-reactid="30"><path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h
